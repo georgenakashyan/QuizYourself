481acfd25bf054bce830f2ea13c7d23259e90edc
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2024-08-10 01:17:37 -0400

    feat: link users and studysets functionality to app

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,12 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
 
+const studysetsRouter = require('./routes/studysets.js');
+const usersRouter = require('./routes/users.js');
+
+app.use('/studysets', studysetsRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
 });
